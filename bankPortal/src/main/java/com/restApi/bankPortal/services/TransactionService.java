ac608955c2b0698516278d91d27d903f03167f44
amoskarugo <karugoamos@gmail.com>
2024-08-28 23:30:10 +0300

    create an interface containing methods to handle all the activities in transactions entity

--- bankPortal/src/main/java/com/restApi/bankPortal/services/TransactionService.java
+++ bankPortal/src/main/java/com/restApi/bankPortal/services/TransactionService.java
@@ -1,5 +1,6 @@
 package com.restApi.bankPortal.services;
 
+import com.restApi.bankPortal.apiResponseHandler.ApiResponse;
 import com.restApi.bankPortal.domain.entities.Transactions;
 
 import java.util.Optional;
@@ -8,4 +9,5 @@ public interface TransactionService {
 
     Optional<Transactions> findByID(Long id);
     Transactions updateTransaction(Transactions transaction);
+    ApiResponse<?> transactionHistory(Long accountNo);
 }
